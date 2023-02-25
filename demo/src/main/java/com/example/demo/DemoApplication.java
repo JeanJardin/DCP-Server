5f8133d9b02c1ff33c45089349153e874c0d9bce
Abdullah <binjabdu@gmail.com>
2023-02-25 22:21:23 +0100

    initial commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/")
+	public String hello(){
+		return "DCP Hashing server is online";
+	}
+	@GetMapping("/HashVideo")
+	public String hashVideo(){
+		return "hashvideo";
+	}
+}
