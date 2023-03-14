0d3b800b785cdc886fb4343a5af8c326da2c8209
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2023-03-14 10:00:02 +0200

    Java doc part 1

--- demo/src/main/java/com/example/demo/controllers/CheckSumController.java
+++ demo/src/main/java/com/example/demo/controllers/CheckSumController.java
@@ -3,8 +3,17 @@ package com.example.demo.controllers;
 import org.springframework.http.ResponseEntity;
 
 
-
+/**
+ * CheckSumController class that implements the IChecksumController interface.
+ * It provides functionality for checking the integrity of content using a checksum.
+ */
 public class CheckSumController implements IChecksumController{
+    /**
+     * {@inheritDoc}
+     * This method checks the integrity of the content using a checksum.
+     * @param contentID the ID of the content to check
+     * @return a ResponseEntity<Boolean> indicating whether the content is intact or not
+     */
     @Override
     public ResponseEntity<Boolean> checkContentIntegrity(String contentID) {
         return null;
--- demo/src/main/java/com/example/demo/controllers/ContentController.java
+++ demo/src/main/java/com/example/demo/controllers/ContentController.java
@@ -15,23 +15,43 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * The ContentController class provides REST endpoints for managing contents.
+ * It is responsible for creating, retrieving, and adding content to the system.
+ */
 @RestController
 public class ContentController {
 
     private final ContentFactory contentFactory;
     private final ContentService contentService;
 
+    /**
+     * Constructs a new instance of the ContentController class with the specified parameters.
+     *
+     * @param contentService the service that manages content
+     * @param contentFactory the factory used for creating content
+     */
     @Autowired
-    public ContentController(ContentService contentService,ContentFactory contentFactory) {
+    public ContentController(ContentService contentService, ContentFactory contentFactory) {
         this.contentService = contentService;
         this.contentFactory = contentFactory;
     }
 
+    /**
+     * Retrieves all contents from the repository and returns them as a list in the response body.
+     *
+     * @return a ResponseEntity<List<Content>> containing all contents
+     */
     @GetMapping("/contentAll")
     public ResponseEntity<List<Content>> getAllContents() {
         return ResponseEntity.ok(this.contentService.getContentRepository().findAll());
     }
 
+    /**
+     * Creates a new content of type "Videos" using the content factory.
+     *
+     * @return a message indicating the completion of the creation process
+     */
     @GetMapping("/createContent")
     public String createContent() {
         contentFactory.createContent("Videos");
