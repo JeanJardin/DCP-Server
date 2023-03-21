80b8bbc51bad2921e09e895d2b10599ee49dcb15
Abdullah <binjabdu@gmail.com>
2023-03-21 11:15:53 +0100

    removed unused imports v2

--- demo/src/test/java/com/example/demo/service/ContentServiceTest.java
+++ demo/src/test/java/com/example/demo/service/ContentServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Content;
-import com.example.demo.model.ContentFactory;
 import com.example.demo.repository.ContentRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
