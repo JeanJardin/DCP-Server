6a44246d79a1a85dfe1090fe46c289505ab35a43
Abdullah <binjabdu@gmail.com>
2023-03-22 10:33:57 +0100

    testcommit

--- src/main/java/com/example/envUtils/DotenvConfig.java
+++ src/main/java/com/example/envUtils/DotenvConfig.java
@@ -4,6 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvBuilder;
 
 public class DotenvConfig {
+    //testcommit
     private static final Dotenv dotenv = new DotenvBuilder().filename("serverParameters.env").ignoreIfMissing().load();
 
     public static String get(String key) {
