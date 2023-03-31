8148ccf5c0f4c6e87688cbbaa0bd338f35184525
Abdullah <binjabdu@gmail.com>
2023-03-31 16:20:17 +0200

    Fixed controllers

--- src/main/java/com/example/demo/controllers/ContentController.java
+++ src/main/java/com/example/demo/controllers/ContentController.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Content;
 import com.example.demo.model.ContentFactory;
 import com.example.demo.service.AirtableService;
 import com.example.demo.service.ContentService;
-import com.example.envUtils.DotenvConfig;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,9 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +22,6 @@ import java.util.Optional;
  */
 @RestController
 public class ContentController {
-
     private final ContentFactory contentFactory;
     private final ContentService contentService;
 
@@ -54,27 +51,24 @@ public class ContentController {
         return "Finished !";
     }
 
-    @GetMapping("/getBinaryHashesFromAirtableID")
-    public ResponseEntity<JSONObject> getBinaryHashesFromAirtableID(@RequestParam("airtableID") String airtableID) throws JSONException {
+    @GetMapping(path = "/getBinaryHashesFromAirtableID", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getBinaryHashesFromAirtableID(@RequestParam("airtableID") String airtableID) throws JSONException {
         Optional<Content> contentFound = contentService.getContentByAirtableId(airtableID);
         System.out.println("Called ! ");
         if (contentFound.isPresent()) {
-            JSONArray jsonArray = new JSONArray(contentFound.get().getBinaryHashes());
-            JSONObject jsonObject  = new JSONObject();
-            jsonObject.put("binaryHashes",jsonArray);
-            System.out.println("JSON OBJECT IS : "+jsonObject);
-            return ResponseEntity.ok(jsonObject);
+            Content content = contentFound.get();
+            return ResponseEntity.status(200).body(content.getBinaryHashes());
         } else {
             return ResponseEntity.notFound().build();
         }
     }
-    @GetMapping(path = "/getJsonHashFromAirtableID", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<JSONObject> getJsonHashFromAirtableID(@RequestParam("airtableID") String airtableID) throws JSONException {
-        Optional<Content> contentFound = contentService.getContentByAirtableId(airtableID);
 
+    @GetMapping(path = "/getJsonHashFromAirtableID", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getJsonHashFromAirtableID(@RequestParam("airtableID") String airtableID) throws JSONException {
+        Optional<Content> contentFound = contentService.getContentByAirtableId(airtableID);
         if (contentFound.isPresent()) {
-            JSONObject jsonObject  = new JSONObject(contentFound.get().getJsonHash());
-            return ResponseEntity.ok(jsonObject);
+            Content content = contentFound.get();
+            return ResponseEntity.ok(content.getJsonHash());
         } else {
             return ResponseEntity.notFound().build();
         }
